a5aaac80eafffbf144daf9fa846c90e9bf0a3b5b
Shogo <vanitas710@yahoo.co.jp>
2021-02-03 16:30:36 +0900

    タップした座標に画像が動くようにする

--- easeInOut/ViewController.swift
+++ easeInOut/ViewController.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var azarashi: UIImageView!
+    
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        
+        // タップされた座標を調べる
+        let tapPoint = sender.location(in: view)
+        
+        // 指定したアニメーションする
+        UIView.animate(withDuration: 1.0,
+                       delay: 0,
+                       options: [.curveEaseInOut],
+                       animations: {
+                        // タップした座標に移動
+                        self.azarashi.center = tapPoint
+                       },
+                       completion: nil
+                       )
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
